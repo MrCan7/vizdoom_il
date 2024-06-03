6c8411e6f4ee3d6b3606f822bf0992228d8249b1
MrCan7 <unluom16@itu.edu.tr>
2024-06-04 02:16:00 +0300

    add default vizdoom scenarios and examples

--- data/parameters.py
+++ data/parameters.py
@@ -1,5 +1,5 @@
 params = {
-    "scenario": "cig",
+    "scenario": "deathmatch",
     "num_bots" : 8,
     "episodes" : 1
 }
--- data/singleplayer_dm.py
+++ data/singleplayer_dm.py
@@ -5,14 +5,18 @@ from time import sleep
 
 if __name__ == "__main__":
     game = vzd.DoomGame()
-
+    ## Load the scenario
+    # Dirty way to get the path to the scenario folder
+    current_file_path = os.path.realpath(__file__)
+    current_directory = os.path.dirname(current_file_path)
+    scenario_directory = os.path.dirname(current_directory) + "/scenarios"
+    
     scenario = params["scenario"] + ".cfg"
-    scenario_path = os.path.join(vzd.scenarios_path, scenario)
-    #scenario_path = "/home/puren/İTÜ_DERSLER/AI/PROJECT/vizdoom_il/scenarios/cig.cfg"
-
+    scenario_path = os.path.join(scenario_directory, scenario)
     game.load_config(scenario_path)
+    
     #game.set_doom_map("map01") #limited deathmatch
-    game.set_doom_map("map02") # full deathmatch. builtin map
+    # game.set_doom_map("map02") # full deathmatch. builtin map
     
     game.set_screen_resolution(vzd.ScreenResolution.RES_640X480)
     game.set_screen_format(vzd.ScreenFormat.RGB24)
@@ -27,7 +31,7 @@ if __name__ == "__main__":
     game.set_audio_sampling_rate(vzd.SamplingRate.SR_22050)
 
     #game.set_living_reward(-1)
-    game.set_mode(vzd.Mode.SPECTATOR)
+    game.set_mode(vzd.Mode.PLAYER)
     #game.set_console_enabled(True)
     game.add_game_args("-record multi_rec.lmp")
 
@@ -37,7 +41,7 @@ if __name__ == "__main__":
     game.add_game_args(
         "-host 1 -deathmatch +timelimit 2.0 "
         "+sv_forcerespawn 1 +sv_noautoaim 1 +sv_spawnfarthest 1 +sv_nocrouch 1 "
-        "+viz_respawn_delay 10 +viz_nocheat 1"
+        "+viz_respawn_delay 1 +viz_nocheat 1"
         
         
     )
